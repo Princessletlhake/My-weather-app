8924a8681653a590aee28c22bcbe9b9c4ae7a312
Princess Letlhake <princessletlhake7@gmail.com>
2024-07-03 00:40:44 +0200

    injected more weather data.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,42 @@ function updateWeatherData(response) {
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+  let desciptionElement = document.querySelector("#description");
+  desciptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    " Monday",
+    "Tuesday",
+    "Wednsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
